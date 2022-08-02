77afa6c618c214bf86ffc177c08c1d65591db56c
Afifatur Rofi'ah <107178739+afifaturrofiah@users.noreply.github.com>
2022-08-02 16:09:04 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost","root","","belajar_multilevel");
+$koneksi = mysqli_connect("localhost","mahadaly","mahadaly123","admin");
 
 // Check connection
 if (mysqli_connect_errno()){
