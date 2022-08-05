21b32e4477174437d74b13e60961482d65cf8ce2
khalilatul06 <107179035+halila06@users.noreply.github.com>
2022-08-05 09:12:16 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost","mahadaly","mahadaly123","admin");
-
-// Check connection
-if (mysqli_connect_errno()){
- echo "Koneksi database gagal : " . mysqli_connect_error();
-}
-
-?>
