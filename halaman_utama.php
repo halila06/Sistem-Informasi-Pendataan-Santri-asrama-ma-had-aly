fdb9914b87d8d830f252519abb241a5b804865ff
Afifatur Rofi'ah <107178739+afifaturrofiah@users.noreply.github.com>
2022-08-09 16:53:55 +0700

    Add files via upload

---
+++ halaman_utama.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/SistemInformasi/Boostrap/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="fontawesome-free-5.15.4-web/fontawesome-free-5.15.4-web/css/all.min.css">
+
+    <title>Asrama Ma'had Aly</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <!---awal navbar --->
+            <div class="container">
+            <a class="navbar-brand" href="#">Asrama<strong>Ma'had Aly</strong></a>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <form action="Datasantri.php" method="post">
+<input type="submit" class="tombol_login" value="Data santri">
+</div>
+            </div>
+        </nav>
+        <!--Akhir Navbar-->
+
+        <!--awal carousel-->
+                <div class="container">
+                    <div id="carouselExampleIndicators" class="carousel slide my-3" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 1"></button>
+                        </div>
+                        <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="/SistemInformasi/img/1.jpg" class="d-block w-100" alt="Gambar 2">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/SistemInformasi/img/kiai.jpg" class="d-block w-100" alt="Gambar 12">
+                        </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+            </div>
+
+            <!--about-->
+            <section id="about">
+              <div class="container">
+                <div class="row text-center mb-3">
+                  <div class="col">
+                    <h2>ABOUT</h2>
+                  </div>
+                </div>
+                <div class="row justify-content-center fs-5 text-center">
+                  <div class="col">
+                    <p>Ma'had Aly adalah salah satu asrama cabang yang berada dalam naungan Pondok Pesantren Salafiyah Syafi'iyah</p>
+                     <p> Ma'had Aly juga asrama yang mengkhususkan santri yang hendak memperdalami kitab </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <!--akhir about-->
+
+        <!--akhir carousel-->
+        <!--footer-->
+            <footer class="bg-light text-dark p-5 mt-5">
+                <div class="row">
+                <div class="col-md-3">
+                    <h5>LAYANAN ASRAMA</h5>
+                    <ul>
+                    <li>KANTOR MA'HAD ALY</li>
+                    <li>CARA PEMBELIAN</li>
+                    <li>PENGIRIMAN</li>
+                    <li>CARA PENGEMBALIAN</li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>TENTANG KAMI</h5>
+                    <p>scarlet shop adalah brand lokal milik artis Felicya Angelista yang berfokus pada produk pencerah kulit. Produk brand ini menawarkan berbagai manfaat seperti mengangkat sel kulit mati dan kotoran yang ada di tubuh kita.
+                    </p>
+                </div>
+                </div>
+                <footer class="bg-light p-5 mt-5">
+                    <div class="container">
+                        <div class="row mt-2">
+                            <div class="col md-6 text-md-start text-center pt-2 pb-2">
+                                <a href="#" class="text-decoration-none">
+                                    <a href="#" >
+                                    <img src="" alt="">
+                                </a>
+                                <span>copy right @2022 | created with <i class="fas fa-heart text-danger"></i> by <a href="https://scarlett-whitening.com/all?gclid=Cj0KCQiArt6PBhCoARIsAMF5wajxn_u7WcwD4nFdw5Vr6Ot3OX6WjgiGL7L_f3CQ7VgQf9ykxrnePFcaApoeEALw_wcB" class="text-decoration-none text-dark fw-bo ">Ma'had Aly Putri</a> </span>
+                           
+                        </div>
+                    </div>
+                </footer>
+            </footer>
+            
+        <!--akhir footer-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
